4635abd8788ceced3a9e7a531230569e5937eb9b
JohnSang16 <johnsang1970@gmail.com>
2026-03-08 12:30:31 -0400

    working sol

--- Neetcode150/Arrays and Hashing/Longest  Consecutive Sequence.py
+++ Neetcode150/Arrays and Hashing/Longest  Consecutive Sequence.py
@@ -1,31 +1,16 @@
-#create a array of binary nums where 1 represents valid 
-#number for a consecutive sequence and 0 represents nonvalid
-#return the num of valid nums
-#check for valid nums by iterating through
+#first check if the num is the starting val of a seq
+#if so check how long the seq is and store it 
+#return the longest
 class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int:
-        binaryArray = [0]*len(nums)
-        
-        for i in range(len(nums)-1): 
-            j = i + 1 
-            while j < len(nums):
-                #bArr = [1, 0, 1, 0, 1, 1] 
-                if binaryArray[i] == 1:
-                    continue
-                elif (nums[i])+1 == nums[j]:
-                    binaryArray[i] = 1
-                    binaryArray[j] = 1
-                    break
-                j += 1
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
 
-            j = 0
-        res = 0
-        for ones in binaryArray:
-            if ones == 1:
-                res += 1 
-        
-        return res
-               
-c = Solution()
-test1 = c.longestConsecutive([2,20,4,10,3,4,5])
-print(test1)
+        for n in nums:
+            if (n-1) not in nums:
+                length = 0
+                while (n + length) in nums:
+                    length += 1 
+                longest = max(length, longest)
+        return longest
+                
